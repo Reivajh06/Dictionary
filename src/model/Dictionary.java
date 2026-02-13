2931d3ef9f4d2d9c92866b1276a4170afce206e9
ReivajH06 <reivajh06@gmail.com>
2026-02-13 10:50:07 +0000

    Added Baselength for the hashtable and Dummy sentinel object for hashtable's buckets states

--- src/model/Dictionary.java
+++ src/model/Dictionary.java
@@ -2,5 +2,23 @@ package model;
 
 public class Dictionary<K, V> {
 
-	
+	private static final int BASELENGTH = 8;
+	private static Object DUMMY = new Object();
+
+	private Object[] hashtable;
+
+	public Dictionary() {
+		hashtable = new Object[BASELENGTH];
+	}
+
+	public void add(K key, V value) {
+		int index = key.hashCode() % hashtable.length;
+
+		if(hashtable[index] == null || hashtable[index].equals(DUMMY)) {
+			hashtable[index] = value;
+			return;
+		}
+
+
+	}
 }
