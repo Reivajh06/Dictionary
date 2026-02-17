b3e40528c6ce7eec90c404beebf2697da82f992f
ReivajH06 <reivajh06@gmail.com>
2026-02-17 15:55:31 +0000

    Applied restriction of the use of the Java Collection Framework. Added items function, constructor with specification of keys and fromkeys function similar to python function

---
+++ src/model/PyDict.java
@@ -0,0 +1,176 @@
+package model;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class PyDict<K, V> {
+
+	/*
+	Functions to add:
+	clear
+	copy
+	fromkeys
+	get
+	pop
+	popitem
+	setdefault
+	 */
+
+	private static final int EXPANSIONFACTOR = 75;
+
+	private static final int DKIXEMPTY = -1;
+	private static final int DKIXDUMMY = -2;
+	private static final int PERTURBSHIFT = 5;
+	private static final int BASELENGTH = 8;
+
+	private int[] indices;
+	private int mask;
+
+	private Entry<K, V>[] entries;
+	private int nextEntryIndex = 0;
+
+	private int nItems;
+
+	public static <K, V> PyDict<K, V> fromKeys(K[] keys, V initialValue) {
+		return new PyDict<>(keys, initialValue);
+	}
+
+	public static <K, V> PyDict<K, V> fromKeys(K[] keys) {
+		return fromKeys(keys, null);
+	}
+
+	public PyDict() {
+		indices = new int[BASELENGTH];
+		fill(indices, -1);
+		entries = new Entry[BASELENGTH];
+		mask = BASELENGTH - 1;
+
+		nItems = 0;
+	}
+
+	/*
+	* Constructor similar to fromkeys function in python to create dictionary with default value
+	*
+	* */
+	public PyDict(K[] keys, V initialValue) {
+		indices = new int[keys.length * 2];
+		fill(indices, -1);
+		entries = new Entry[keys.length * 2];
+
+		mask = keys.length - 1;
+
+		nItems = 0;
+
+		for (K key : keys) {
+			put(key, initialValue);
+		}
+	}
+
+	public PyDict(K[] keys){
+		this(keys, null);
+	}
+
+	private void grow() {
+		Entry<K, V>[] entr = new Entry[entries.length];
+		System.arraycopy(entries, entries.length, entr, 0, entries.length);
+
+		entries = new Entry[entries.length * 2];
+		indices = new int[indices.length * 2];
+
+		fill(indices, -1);
+
+		mask = indices.length - 1;
+		nItems = 0;
+		nextEntryIndex = 0;
+
+		for(Entry<K, V> entry : entr) {
+			if(entry == null) continue;
+			put(entry.key, entry.value);
+		}
+	}
+
+	private void fill(int[] arr, int initialValue) {
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = initialValue;
+		}
+	}
+
+	public void put(K key, V value) {
+		if((nItems * 100) / indices.length >= EXPANSIONFACTOR) {
+			grow();
+		}
+
+		//Remember to include a reference to the first dummy found,
+		//Finding a dummy may not lead to insertion as said key might already exists
+		int firstDummyPos = -1;
+
+		int perturb = key.hashCode();
+		int j = perturb;
+		int index = j & mask;
+
+		while(true) {
+			if(firstDummyPos == -1) {
+				if(indices[index] == DKIXDUMMY) firstDummyPos = index;
+
+				else if(indices[index] == DKIXEMPTY) break;
+
+			} else {
+				if(indices[index] == DKIXEMPTY) {
+					index = firstDummyPos;
+					break;
+				}
+			}
+
+			if(entries[indices[index]].key.equals(key)) {
+				entries[indices[index]] = new Entry<>(key, value, key.hashCode());
+				return;
+			}
+
+			//unsigned shift
+			perturb >>>= PERTURBSHIFT;
+			j = (5 * j) + 1 + perturb;
+			index = j & mask;
+		}
+
+		entries[nextEntryIndex] = new Entry<>(key, value, key.hashCode());
+		indices[index] = nextEntryIndex++;
+		nItems++;
+	}
+
+	public void update(PyDict<K, V> otherDict) {
+		for(Pair<K, V> pair : otherDict.items()) {
+			put(pair.key, pair.value);
+		}
+	}
+
+	public K[] keys() {
+		return (K[]) Stream.of(entries)
+				.filter(Objects::nonNull)
+				.map(Entry::key)
+				.toArray(Object[]::new);
+	}
+
+	public V[] values() {
+		return (V[]) Stream.of(entries)
+				.filter(Objects::nonNull)
+				.map(Entry::value)
+				.toArray(Object[]::new);
+	}
+
+	public Pair<K, V>[] items() {
+		return (Pair<K, V>[]) Stream.of(entries)
+				.filter(Objects::nonNull)
+				.map(e -> new Pair<>(e.key, e.value))
+				.toArray(Pair[]::new);
+	}
+
+	record Entry<K, V>(K key, V value, int hash) {
+	}
+
+	public record Pair<K, V>(K key, V value) {
+
+		public String toString() {
+			return "[%s, %s]".formatted(key, value);
+		}
+	}
+}
